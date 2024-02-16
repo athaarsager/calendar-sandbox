8519bd08323bbfa4c024e685a88c3688a65e4e74
Andrew Haarsager <athaarsager@gmail.com>
2024-02-16 14:15:49 -0600

    Added extra comment

--- src/Calendar/CalendarDisplay.jsx
+++ src/Calendar/CalendarDisplay.jsx
@@ -6,7 +6,8 @@ export default class CalendarDisplay extends React.Component {
   render() {
     return (
       // Calendar will always take up its entire container width 
-      //Can manually set height via props
+      // Can manually set height via props
+      // Can also use aspectRatio to adjust height
       // Apparently you don't even need to re-size the height if you have the width selected
       <div className="calendar-container">
         <FullCalendar
