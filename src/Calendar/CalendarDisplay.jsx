55a2232cd946ffd4259faa7d7f56a88fa11aae30
Andrew Haarsager <athaarsager@gmail.com>
2024-02-16 14:14:36 -0600

    Added notes for documentation

--- src/Calendar/CalendarDisplay.jsx
+++ src/Calendar/CalendarDisplay.jsx
@@ -5,12 +5,14 @@ import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 export default class CalendarDisplay extends React.Component {
   render() {
     return (
-      // Calendar will always take up its entire container width, but can manuall set height
+      // Calendar will always take up its entire container width 
+      //Can manually set height via props
+      // Apparently you don't even need to re-size the height if you have the width selected
       <div className="calendar-container">
         <FullCalendar
           plugins={[dayGridPlugin,]}
           initialView="dayGridMonth"
-          height="50vh"
+          // height="50vh"
         />
       </div>
     )
